b0135946bed87763a396fe100b7e0d9ea243eb66
Anele Mbatha <collenmbatha40@gmail.com>
2023-01-10 02:58:38 -0800

    function that adds 2 integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+"""
+This module is composed by a function that adds two numbers
+"""
+
+
 def add_integer(a, b=98):
-    """function that add 2 numbers"""
+    """Function that adds two integer and/or float numbers"""
 
     if not a or (type(a) != int and type(a) != float):
         raise TypeError("a must be an integer")
     if type(b) != int and type(b) != float:
         raise TypeError("b must be an integer")
-
-    return(int(a) + int(b))
+    a = int(a)
+    b = int(b)
+    return (a + b)
