fc0f6d0248ec4e2562b2bbfd7440a65efb0e4592
Anele Mbatha <collenmbatha40@gmail.com>
2023-03-28 06:58:09 -0700

    Fixed task 0 files

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,10 +7,12 @@ This module is composed by a function that adds two numbers
 def add_integer(a, b=98):
     """Function that adds two integer and/or float numbers"""
 
-    if not a or (type(a) != int and type(a) != float):
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if type(b) != int and type(b) != float:
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
-    return (a + b)
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
