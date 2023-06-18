07c90bab42907654d59ecdd78a06bb64e613621c
Anele Mbatha <collenmbatha40@gmail.com>
2023-06-18 04:23:44 -0700

    Added files for task 0 and 1

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Define a rectangle"""
+
+class Rectangle:
+    """Represents a rectangle"""
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""Define a class called Rectangle"""
+
+class Rectangle:
+    """Initializes a rectangle"""
+
+    def __init__(self, width = 0, height = 0):
+        """Initializing the rectangle dimensions"""
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """retrieves the value for width"""
+        return self.__width
+    
+    @width.setter
+    def(self, value):
+        """sets the value of width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+    
+    @property
+    def height(self):
+        """retrieves the value for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets the value of height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
